a6e7521bfbe826c6623d075a2c545679f96a4e99
Golap-mostofa <mostofa6410@gmail.com>
2022-04-13 16:50:03 +0600

    Login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigete = useNavigate()
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+   
+    const nevigetRegestar = ()=>{
+        navigete('/signUp')
+    }
+
+    const hendelSubmit = e =>{
+        e.preventDefault()
+        // const email = emailRef.current.value
+        // const password = passwordRef.current.value 
+        // console.log(email,password);
+
+
+    }
+
+
     return (
-        <div>
-            <h2>Login</h2>
-            <Form>
+        <div className='container w-50 mx-auto mt-5'>
+            <h1 className='text-dark text-center'>Pleash Login</h1>
+            <Form onSubmit={hendelSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -16,7 +35,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -25,6 +44,7 @@ const Login = () => {
                     Submit
                 </Button>
             </Form>
+            <p>New to Genius Car <Link to={'/signUp'} className='text-danger text-decoration-none' onClick={nevigetRegestar}>Pleash SignUp</Link></p>
         </div>
     );
 };
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Link, useNavigate, } from 'react-router-dom';
+import './SignUp.css'
 const SignUp = () => {
+    const navivate = useNavigate()
+    const nevigetSignUp = () => {
+        navivate('/login')
+    }
+    const hendelSignUp = event => {
+        event.preventDefault()
+        const name = event.target.name.value
+        const email = event.target.email.value
+        const password = event.target.password.value
+        console.log(name, email, password);
+
+
+
+    }
     return (
-        <div>
-            <h2>SignUp</h2>
+        <div className='signUp-form mt-5'>
+            <h1 className='text-center text-primary mb-3'>Create account</h1>
+
+            <form onSubmit={hendelSignUp} className='from-container'>
+                <h3 className='text-center text-dark'>SignUp</h3>
+                <input type="text" name="name" id="" placeholder='Your name' /><br />
+                <input type="email" name="email" id="" required placeholder='your email' /><br />
+                <input type="password" name="password" id="" required placeholder='password' /><br />
+                <input id='submit' type="submit" value="SignUp" />
+                <>
+                    <p className='text-center'>New to Genius Car <Link to={'/login'} className='text-danger text-decoration-none' onClick={nevigetSignUp}>Pleash SignUp</Link></p>
+                </>
+            </form>
+
+
         </div>
     );
 };
